8c6d92ecd3d808eb7f471b6086a466100a0103c8
JERRYNSK <jerryoliveira909@gmail.com>
2025-06-22 17:21:28 -0300

    first commit

---
+++ Game.py
@@ -0,0 +1,52 @@
+import pygame
+from Player import Player
+pygame.init()
+#  variables
+width=800
+height=480
+screen = pygame.display.set_mode((width,height))
+red =(255,0,0)
+x_pos_player = width/2
+y_pos_player = height/2
+
+
+#tem que criar uma classe pra usar o sprite... merda
+
+jogador = Player()
+pl_group = pygame.sprite.Group()
+pl_group.add(jogador)
+
+print('teste de git')
+
+#functions
+def update_game():
+    global x_pos_player
+    global y_pos_player
+ 
+
+    rotate_player()
+    jogador.update_pos(x_pos_player, y_pos_player)
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        x_pos_player -= 1
+    if keys[pygame.K_w]:
+        y_pos_player-=1
+    if keys[pygame.K_s]:
+        y_pos_player+=1
+    if keys[pygame.K_d]:
+        x_pos_player+=1
+    pl_group.update(screen)
+def rotate_player():
+    xm,ym = pygame.mouse.get_pos()
+    jogador.rotate(xm, ym, x_pos_player, y_pos_player)
+
+def draw_game():
+    pl_group.draw(screen)
+    pygame.display.update()
+while True:
+    screen.fill((0,0,0))
+    update_game()
+    draw_game()
---
+++ Player.py
@@ -0,0 +1,40 @@
+import pygame
+import math
+class Player(pygame.sprite.Sprite):
+    direction = 1
+    caminho_imagem = pygame.image.load('/home/jerry/Downloads/idle.png')
+    def __init__(self):
+        global caminho_imagem 
+        caminho_imagem = pygame.image.load('/home/jerry/Downloads/idle.png')
+        pygame.sprite.Sprite.__init__(self)
+        self.image = caminho_imagem.convert()
+        self.image.fill((0, 0, 255))  # cor verde
+        self.rect = self.image.get_rect()
+        self.rect.center = (100, 100)  # posição inicial
+    def update_pos(self, x, y):
+        self.rect.center=(x,y)
+     
+    def rotate(self, xm, ym, xp, yp):
+        global direction
+        new_x = xm - xp
+        new_y = ym - yp
+        angle = math.degrees(math.atan2(-new_y,new_x))
+        #self.image = pygame.transform.rotate(caminho_imagem, -angle )
+        
+        #flipar se passar dos eixos:3
+        #ta dando errado essa porra, deixa um top down mesmo
+        if 180 > angle > 90 or -180 < angle < -90:
+          
+            self.image = pygame.transform.rotate(caminho_imagem, -angle )
+            self.image = pygame.transform.flip(self.image, False, True) 
+            self.rect = self.image.get_rect(center=self.rect.center)
+        
+        else:
+            
+            self.image = pygame.transform.rotate(caminho_imagem, angle )
+            self.image = pygame.transform.flip(self.image, False, False) 
+            self.rect = self.image.get_rect(center=self.rect.center)
+
+        
+            
+        #print(angle)
